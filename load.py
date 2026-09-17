cd4f2e0546256060ffd5b68ecade0b63fe08fa43
Cameron Berryman <91347395+CB12354@users.noreply.github.com>
2026-09-17 12:52:03 +0000

    emissions

--- load.py
+++ load.py
@@ -2,6 +2,13 @@ import duckdb
 import os
 import logging
 
+#Complete the load.py script to create a local, persistent DuckDB database that creates and loads (at most) three tables:
+
+# A full table of YELLOW taxi trips for all of 2024.
+# A full table of GREEN taxi trips for all of 2024.
+# A lookup table of vehicle_emissions based on the included CSV file above.
+
+
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
     filename='load.log'
@@ -18,9 +25,29 @@ def load_parquet_files():
         logger.info("Connected to DuckDB instance")
 
         con.execute(f"""
-            -- SQL goes here
+            DROP TABLE IF EXISTS vehicle_emissions;
+            DROP TABLE IF EXISTS yellow_trips;
+            DROP TABLE IF EXISTS green_trips;
+            CREATE TABLE vehicle_emissions AS 
+            SELECT * FROM read_csv_auto(
+                'data/vehicle_emissions.csv');
         """)
         logger.info("Dropped table if exists")
+        n = con.execute("SELECT COUNT(*) FROM vehicle_emissions").fetchone()[0]
+        logger.info(f"vehicle_emissions: {n} rows loaded")
+#        for color_vars in [('yellow', "t"), ('green',"l")]:
+#            for month in range(1, 13):
+#                url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{color_vars[0]}_tripdata_2024-{month:02d}.parquet'
+#                con.execute(f"""
+#                    INSERT INTO {color_vars[0]}_trips 
+#                    SELECT 
+#                        VendorID, 
+#                        {color_vars[1]}pep_pickup_datetime AS pickup_time, 
+#                        {color_vars[1]}pep_dropoff_datetime AS dropoff_time, 
+#                        passenger_count, 
+#                        trip_distance 
+#                    FROM read_parquet('{url}');
+#                            """)
 
     except Exception as e:
         print(f"An error occurred: {e}")

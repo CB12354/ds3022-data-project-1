d2d8596290dc13e24cfc5b0023e53c8bdb55b487
Cameron Berryman <91347395+CB12354@users.noreply.github.com>
2026-09-18 13:37:14 +0000

    clean and transform, add SNS requirement

--- clean.py
+++ clean.py
@@ -12,12 +12,43 @@ import logging
 con = None
 try:
     con = duckdb.connect(database="emissions.duckdb",read_only=False)
-    print(con.execute("""
-                SELECT DISTINCT MONTH(pickup_time) AS month FROM yellow_trips ORDER BY month;
-                """).fetchall())
-    print(con.execute("""
-                SELECT DISTINCT MONTH(pickup_time) AS month FROM green_trips ORDER BY month;
-                """).fetchall())
+    # Drop duplicates
+    for color in ['yellow','green']:
+        print(f"Taxi color: {color}")
+        con.execute(f"DROP TABLE IF EXISTS {color};")
+        con.execute(f"""CREATE TABLE {color} AS SELECT DISTINCT * FROM {color}_trips;""")
+        # Check there are no duplicate rows in color
+        print(con.execute(f"""SELECT COUNT(*) FROM {color}_trips;""").fetchone()[0])
+        print(con.execute(f"""SELECT COUNT(*) FROM {color};""").fetchone()[0])
+        
+        # Drop 0-passenger trips
+        con.execute(f"""DELETE FROM {color} WHERE passenger_count = 0;""")
+        print(con.execute(f"""SELECT COUNT(*) FROM {color};""").fetchone()[0])
+        
+        # Drop trips of 0 miles and over 100 miles
+        con.execute(f"DELETE FROM {color} WHERE trip_distance = 0 OR trip_distance > 100;")
+        print(con.execute(f"""SELECT COUNT(*) FROM {color};""").fetchone()[0])
+        
+        # Drop trips over 1 day long
+        con.execute(f"DELETE FROM {color} WHERE DATE_DIFF('second', pickup_time, dropoff_time) > 86400;")
+        print(con.execute(f"""SELECT COUNT(*) FROM {color};""").fetchone()[0])
+        
+        #double-check there are no rows with the removed conditions
+        print(con.execute(f"""SELECT COUNT(*) FROM {color} WHERE
+                          passenger_count = 0 OR
+                          trip_distance = 0 OR
+                          trip_distance > 100 OR
+                          DATE_DIFF('second', pickup_time, dropoff_time) > 86400;""").fetchone()[0])
+        
+        # Replace the original table with the new one, then discard the new one
+        con.execute(f"""
+                    DELETE FROM {color}_trips;
+                    INSERT INTO {color}_trips SELECT * FROM {color};
+                    DROP TABLE {color};""")
+        print(con.execute(f"""SELECT COUNT(*) FROM {color}_trips;""").fetchone()[0])
+        
+    
+    
 except Exception as e:
     print(f"Caught exception: " + str(e))
     exit()
--- transform.py
+++ transform.py
@@ -1,2 +1,52 @@
 import duckdb
 import logging
+import matplotlib.pyplot as plt
+
+def newcol(con, color, name, dtype):
+    con.execute(f"""ALTER TABLE {color}_trips
+                        ADD COLUMN IF NOT EXISTS {name} {dtype};""")
+
+try:
+    con = duckdb.connect(database="emissions.duckdb",read_only=False)
+    for color in ['yellow','green']:
+        # emissions per trip
+        # Add column to (color)_trips table
+        # trip distance * co2 per mile for respective taxi type
+        newcol(con, color, "trip_co2_kgs", "FLOAT")
+        con.execute(f"""UPDATE {color}_trips
+                        SET trip_co2_kgs = trip_distance *
+                            (SELECT co2_grams_per_mile FROM vehicle_emissions
+                            WHERE vehicle_type = '{color}_taxi') / 1000.0""")
+        print(con.execute(f"""SELECT trip_distance, trip_co2_kgs 
+                          FROM {color}_trips LIMIT 3""").fetchall())
+        # Average mph
+        newcol(con, color, "avg_mph", "FLOAT")
+        con.execute(f"""UPDATE {color}_trips
+                    SET avg_mph = trip_distance / (DATE_DIFF('second', pickup_time, dropoff_time) / 3600.0);""")
+        print("Average mph")
+        print(con.execute(f"""SELECT trip_distance, pickup_time, dropoff_time, avg_mph 
+                    FROM {color}_trips LIMIT 3""").fetchall())
+        # Date columns
+        colnames = ["hour_of_day", "day_of_week", "week_of_year", "month_of_year"]
+        cmd = ["DATE_PART('hour', pickup_time)", "DAYOFWEEK(pickup_time)", 
+               "WEEKOFYEAR(pickup_time)", "MONTH(pickup_time)"]
+        for colname, cmd in zip(colnames, cmd):
+            newcol(con, color, colname, "INTEGER")
+            con.execute(f"""UPDATE {color}_trips 
+                        SET {colname} = {cmd};""")
+            print(f"{color} {colname}")
+            print(con.execute(f"""SELECT pickup_time, {colname} 
+            FROM {color}_trips ORDER BY RANDOM() LIMIT 3""").fetchall())
+        
+        # Time plot
+        # sum CO2 totals group by month
+        df = con.execute(f"""SELECT month_of_year, SUM(trip_co2_kgs)
+                         FROM {color}_trips 
+                         GROUP BY month_of_year 
+                         ORDER BY month_of_year ASC""").fetchall()
+        print(df)
+        
+        
+        
+except Exception as e:
+    print(f"Caught exception: {e}")
